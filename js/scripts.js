e95684ff92743d00673fccb09f9adf33da2cb721
marneymallory <79782193+marneymallory@users.noreply.github.com>
2021-04-07 15:21:25 -0800

    wrote first test
    co-authored-by: Adrian Camacho <adriancamacho18@gmail.com>
    Co-authored-by: Vanessa Su <vnessa.su@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,40 +1,30 @@
-$(document).ready(function() {
-  $("#userName").submit(function(event) {
-    event.preventDefault();
-  })
-})
-function Player(XO, username) {
-  this.XO = XO
+// Business Logic for Player -----
+function Player(playerSymbol, username) {
+  this.symbol = playerSymbol;
   this.username = username
 }
+
+Player.prototype.isMyTurn = function(fact) {
+  this.myTurn = fact
+} 
+
+// Business Logic for Game -----
 function Game(players, gameboard) {
   this.players = players
   this.gameboard = gameboard
 }
-function Gameboard(squares) {
-  this.squares = squares
-}
-function Square() {
-this.markedWithSymbol = ""
-}
-Square.prototype.addMark = function(markBeingPassedIntoMethod) {
-  this.markedWithSymbol = markBeingPassedIntoMethod
-}
-Player.prototype.isMyTurn = function(fact) {
-  this.myTurn = fact
-} 
+
 Game.prototype.isItOver = function() {
-  //Gameboard method
   const gameboardObject = this.gameboard;
   const xPlayerSquaresFilled = gameboardObject.squaresFilledByPlayer("X");
   const oPlayerSquaresFilled = gameboardObject.squaresFilledByPlayer("O");
   const emptySquares = gameboardObject.squaresFilledByPlayer("");
-
+  
   const xPlayerString = xPlayerSquaresFilled.toString();
   
   const winningPossibilites = [[1,2,3] [1,4,7] [1,5,9] [4,5,6] [7,8,9] [3,5,7] [2,5,8] [3,6,9]];
   let winningArray = [];
-
+  
   winningPossibilites.forEach(function(possibility) {
     //possibility = [1, 2, 3];
     if(xPlayerSquaresFilled.includes(possibility[0]) && xPlayerSquaresFilled.includes(possibility[1]) && xPlayerSquaresFilled.includes(possibility[2])) {
@@ -48,7 +38,11 @@ Game.prototype.isItOver = function() {
     }
   })
   return "Continue";
+}
 
+// Business Logic for Gameboard -----
+function Gameboard(squares) {
+  this.squares = squares
 }
 
 Gameboard.prototype.squaresFilledByPlayer = function(markedWithSymbol){
@@ -62,6 +56,18 @@ Gameboard.prototype.squaresFilledByPlayer = function(markedWithSymbol){
   });
   return squareNumbersFilled; //array of square numbers that have the markedWithSymbol character
 }
-//Gameboard method
-//look at the Squares
-//return gameboard state
+
+// Business Logic for Square -----
+function Square() {
+  this.markedWithSymbol = ""
+}
+Square.prototype.addMark = function(markBeingPassedIntoMethod) {
+  this.markedWithSymbol = markBeingPassedIntoMethod
+}
+
+// UI Logic -----
+$(document).ready(function() {
+  $("#userName").submit(function(event) {
+    event.preventDefault();
+  })
+})
